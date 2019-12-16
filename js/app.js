8446240c09efa7409dd2de11f353d51bb970a116
Pavel <rogozhin.p@yandex.ru>
2019-12-16 17:37:16 +0300

    ng-repeat example

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 var app = angular.module('expensesApp', []);
 
 app.controller('ExpensesViewController', ['$scope', function($scope){
-  $scope.name = 'Pablo';
-
-  $scope.expense = {
-    description: 'food',
-    amount: 10
-  };
-
-  $scope.phrase = 'the sky is blue';
-
-  $scope.increaseAmount = function(){
-    $scope.expense.amount++;
-  }
+  $scope.items = [
+    {
+      name: 'pizza',
+      ingredients: ['cheese', 'tomato', 'oregano', 'salt']
+    },
+    {
+      name: 'tortilla',
+      ingredients: ['butter', 'salt', 'pepper', 'garlic']
+    },
+    {
+      name:'cake',
+      ingredients:['cream', 'sugar']
+    },
+    {
+      name: 'empadana',
+      ingredients:['flour', 'meat', 'onion']
+    }
+  ];
 }]);
