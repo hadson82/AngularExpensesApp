31b9efcc3d650033d68193cf240bbae9b56145e7
Pavel <rogozhin.p@yandex.ru>
2019-12-16 16:27:42 +0300

    change var value by button in browser

--- js/app.js
+++ js/app.js
@@ -2,4 +2,15 @@ var app = angular.module('expensesApp', []);
 
 app.controller('ExpensesViewController', ['$scope', function($scope){
   $scope.name = 'Pablo';
+
+  $scope.expense = {
+    description: 'food',
+    amount: 10
+  };
+
+  $scope.phrase = 'the sky is blue';
+
+  $scope.increaseAmount = function(){
+    $scope.expense.amount++;
+  }
 }]);
