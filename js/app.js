ac9d578a8b89705b2b1d5d2f15d200a6619a203a
Pavel <rogozhin.p@yandex.ru>
2019-12-16 21:09:57 +0300

    get problem with home page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('expensesApp', ['ngRoute']);
 
-app.config(function($routeProvider){
+app.config('$routeProvider', function($routeProvider){
   $routeProvider
     .when('/', {
       templateUrl: 'views/expenses.html',
@@ -28,18 +28,24 @@ app.controller('HomeViewController', ['$scope', function($scope){
 
 }]);
 
-app.controller('ExpensesViewController', ['$scope', function($scope){
-  $scope.expenses = [
+app.factory('Expenses', function(){
+  var service = {};
+  service.entries = [
     {description: 'food', amount: 10, date: '2019-10-01'},
     {description: 'tickets', amount: 11, date: '2019-10-02'},
     {description: 'food', amount: 12, date: '2019-10-03'},
     {description: 'phone credit', amount: 13, date: '2019-10-04'},
     {description: 'bills', amount: 14, date: '2019-10-05'},
     {description: 'food', amount: 15, date: '2019-10-06'},
-  ]
+  ];
+  return service;
+});
+
+app.controller('ExpensesViewController', ['$scope', 'Expenses', function($scope){
+  $scope.expenses = Expenses.entries;
 }]);
 
-app.controller('ExpenseViewController', ['$scope', '$routeParams',
- function($scope, $routeParams){
-  $scope.someText = 'The world is round. ID=' + $routeParams.id;
-}]);
+// app.controller('ExpenseViewController', ['$scope', '$routeParams',
+//  function($scope, $routeParams){
+//   $scope.someText = 'The world is round. ID=' + $routeParams.id;
+// }]);
